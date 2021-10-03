254aa42c4671ce8daf3687b55b75333832618786
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-03 10:03:42 -0300

    APP FUSION Primeiro Commit Aula 51 criando a primeira class based view: ViewTemplate index.html

--- fusion/settings.py
+++ fusion/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fusion',
-        'USER': 'admin',
-        'PASSWORD': '1234',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'fusion',
+    #     'USER': 'admin',
+    #     'PASSWORD': '1234',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+    # }
 }
 
 
